5f6d25d0b95d09c1bbc1d5159d1ec281b052b7f4
NguyenTruongSon <nguyentruongsonpq2004@gmail.com>
2026-03-19 10:44:23 +0700

    bai buoi 6

---
+++ src/main/java/com/example/NguyenTruongSon_2280602760/NguyenTruongSon2280602760Application.java
@@ -0,0 +1,13 @@
+package com.example.NguyenTruongSon_2280602760;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTruongSon2280602760Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTruongSon2280602760Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenTruongSon_2280602760/NguyenTruongSon2280602760ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenTruongSon_2280602760;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenTruongSon2280602760ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
